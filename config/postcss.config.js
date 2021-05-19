d35b24f43499097e4c30475e314b7623e31213be
Pratik Gajjar <pmgajjar25@gmail.com>
2021-05-20 00:16:31 +0530

    Upgrade to doks v0.2

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -7,6 +7,8 @@ module.exports = {
     autoprefixer(),
     purgecss({
       content: [
+        './node_modules/@hyas/doks/layouts/**/*.html',
+        './node_modules/@hyas/doks/content/**/*.md',
         './layouts/**/*.html',
         './content/**/*.md',
       ],
@@ -19,10 +21,12 @@ module.exports = {
         'th',
         'td',
         ...whitelister([
-          './assets/scss/components/_doks.scss',
-          './assets/scss/components/_code.scss',
-          './assets/scss/components/_search.scss',
-          './assets/scss/common/_dark.scss',
+          './node_modules/@hyas/doks/assets/scss/common/_variables.scss',
+          './node_modules/@hyas/doks/assets/scss/components/_doks.scss',
+          './node_modules/@hyas/doks/assets/scss/components/_code.scss',
+          './node_modules/@hyas/doks/assets/scss/components/_buttons.scss',
+          './node_modules/@hyas/doks/assets/scss/components/_search.scss',
+          './node_modules/@hyas/doks/assets/scss/common/_dark.scss',
           './node_modules/katex/dist/katex.css',
         ]),
       ],
--- content/blog/_index.md
+++ content/blog/_index.md
@@ -1,6 +1,6 @@
 ---
-title: "Backend Blog"
-description: "Lets learn about backend engineering."
+title: "Blog"
+description: "The Doks Blog."
 date: 2020-10-06T08:49:55+00:00
 lastmod: 2020-10-06T08:49:55+00:00
 draft: false
